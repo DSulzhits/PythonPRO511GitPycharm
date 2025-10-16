7e0e003153fdb40744651bc7558081b66b754f8d
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2025-10-16 20:09:43 +0300

    Revert "Merge branch 'feature2' into develop"
    This reverts commit a2065b356778bf5e082db4c6984a72c05a06c4b2, reversing
    changes made to baa9a4f903b4042ba5d084b81b4c642e704c8e3f.
    
    Abort merge develop into main

--- script.py
+++ script.py
@@ -12,14 +12,6 @@ def divide(a, b):
     return f'На ноль делить нельзя!'
 
 
-def exponent(a, b):
-    if type(b) is int:
-        return a ** b
-    return f'Степень может быть только целым числом!'
-
-
 if __name__ == '__main__':
     print(add(3, 5))
     print(multiply(3, 5))
-    print(divide(10, 2))
-    print(exponent(4, 3))
