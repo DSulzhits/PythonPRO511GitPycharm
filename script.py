baa9a4f903b4042ba5d084b81b4c642e704c8e3f
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2025-10-16 20:01:11 +0300

    Add function divide

--- script.py
+++ script.py
@@ -6,6 +6,12 @@ def multiply(a, b):
     return a * b
 
 
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return f'На ноль делить нельзя!'
+
+
 if __name__ == '__main__':
     print(add(3, 5))
     print(multiply(3, 5))
