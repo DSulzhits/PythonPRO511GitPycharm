4462f9c33a45744b74ccf840bb9cd81a0eb7c5e5
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2025-10-16 20:02:27 +0300

    Add function exponent

--- script.py
+++ script.py
@@ -6,6 +6,13 @@ def multiply(a, b):
     return a * b
 
 
+def exponent(a, b):
+    if type(b) is int:
+        return a ** b
+    return f'Степень может быть только целым числом!'
+
+
 if __name__ == '__main__':
     print(add(3, 5))
     print(multiply(3, 5))
+    print(exponent(4, 3))
